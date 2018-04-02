de9f298d5b9c8ebcb5266b695c4caf229ee23bd8
Andrey Oleinikov <dyt321@ya.ru>
2018-04-02 22:53:39 +0300

    Fixed variable name and exits

--- pprint_json.py
+++ pprint_json.py
@@ -5,27 +5,25 @@ import sys
 
 def load_data(filepath):
     if os.path.exists(filepath):
-        with open(filepath, 'r', encoding='utf-8') as f:
+        with open(filepath, 'r', encoding='utf-8') as json_file:
             try:
-                return json.load(f)
+                return json.load(json_file)
             except json.decoder.JSONDecodeError:
                 return None
     return None
 
 
-def pretty_print_json(json_data):
-    print(json.dumps(json_data, indent=4, ensure_ascii=False))
+def pretty_print_json(data_from_json):
+    print(json.dumps(data_from_json, indent=4, ensure_ascii=False))
 
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print('No path specified')
-        exit()
+        sys.exit('No path specified')
 
     filepath = sys.argv[1]
     data_from_json = load_data(filepath)
     if data_from_json is None:
-        print('Wrong file or path')
-        exit()
+        sys.exit('Wrong file or path')
 
     pretty_print_json(data_from_json)
